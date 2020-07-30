7ad8b95f0ba9cb7647a820ee5e737e70f0b7f3fa
Patryk <p.jachimowski89@gmail.com>
2020-07-30 09:11:17 +0200

    Added get data fn to Weather component

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
+require('dotenv').config();
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY
 const express = require('express');
 const request = require('request');
 const app = express();
 
+function bulidURL (inputCity) {
+  return `http://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${WEATHER_API_KEY}`;
+}
+
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
+  const city = req.query.city;
+  request(bulidURL(city), (err, response, body) => {
+    const weatherJSON = JSON.parse(body);
+    console.log('PATTTT', weatherJSON);
+    res.send(weatherJSON);
+  });
 });
+
 
 const port = 5000;
 app.listen(port, () => {
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function Weather () {
-  return (
-    <div> I am Weather</div>
-  );
+class Weather extends Component {
+  state = {
+    lon: '',
+  };
+
+  getDate = () => {
+    axios
+      .get(`http://localhost:5000/?city=${this.props.searchItem}`)
+      .then((res) => res.data)
+      .then((data) => {
+        this.setState({ lon: data.coord.lon });
+      });
+  };
+
+  componentDidMount() {
+    this.getDate();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.searchItem !== prevProps.searchItem) {
+      this.getDate();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.lon}</p>
+      </div>
+    );
+  }
 }
 
 export default Weather;
