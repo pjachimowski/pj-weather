4b3e06414c1acedd606121ba5d76808b30f412cc
Patryk <p.jachimowski89@gmail.com>
2020-07-30 09:21:31 +0200

    search input added to Container Component

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import Weather from './Weather';
 
-function Container() {
-  return (
-    <div>
-      <div> I am Container</div>
-      <Header />
-      <Weather />
-    </div>
-  );
+class Container extends Component {
+  state = {
+    searchItem: 'Auckland',
+  };
+
+  componentDidMount() {
+    document.querySelector('#btn').addEventListener('click', (event) => {
+      let value = document.getElementById('btn').value;
+      this.setState({ searchItem: value });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div> I am Container</div>
+        <Header />
+
+        <Weather searchItem={this.state.searchItem} />
+
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            id="btn"
+            placeholder="Search..."
+          />
+          <button type="submit" id="btn">
+            CLICK HERE
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Container;
